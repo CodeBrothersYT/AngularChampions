bec9d6232359a1e3fe8ad33bc64a2f55085f8ae9
CodeBrothersYT <ytcodebrothers@gmail.com>
2023-06-24 22:35:43 +0200

    add basic table and structure

--- AngularChampionsFE/src/app/champion.ts
+++ AngularChampionsFE/src/app/champion.ts
@@ -1,3 +1,16 @@
+
 export class Champion {
 
+  id: number;
+  firstName: string;
+  lastName: string;
+  emailId: string;
+
+
+  constructor(id: number, firstName: string, lastName: string, emailId: string) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailId = emailId;
+  }
 }
